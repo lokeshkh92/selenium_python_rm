020188e0e94fab3af1510d5d41d18f8b400c6826
Lokesh Khandelwal <lkhandelwal.92@gmail.com>
2018-08-14 21:16:47 +0530

    Search application on differenet pages

--- selenium_rm_logs.py
+++ selenium_rm_logs.py
@@ -4,19 +4,20 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+import time
 
 # resource manager ipaddres and port
 ipaddress = "10.0.10.36"
 rmport = "8088"
 # applicationId for which logs to be analyzed
-#applicationId = "application_1533123882731_0127"
-applicationId = "application_1733123882731_0127"
+applicationId = "application_1533123882731_0127"
+#applicationId = "application_1733123882731_0127"
 
 # launch chrome browser and open resource manager UI
 browser = webdriver.Chrome()
 browser.get('http://{}:{}/cluster'.format(ipaddress, rmport))
 table_id = browser.find_element_by_id("apps")
-table_tab = 1
+table_tab = 2
 
 # Function to count row number for the application id
 def rownum():
@@ -33,20 +34,25 @@ def rownum():
     return row_count,app_flag
 
 
-# Get row number and application found flag status
+# Search application on 1st page
 row_number,app_flag = rownum()
 
+# Search for application in subsequent tabs
 while app_flag == 0:
-    table_tab += 1
-    tab_path = '//*[@id="apps_paginate"]/span/a[' + str(table_tab) + ']'
-    next_table_tab = browser.find_element(By.XPATH, tab_path).click()
+    #tab_path = '//*[@id="apps_paginate"]/span/a[' + str(table_tab) + ']'
+    tab_path = '//*[@id="apps_next"]'
+    next_table_tab = browser.find_element(By.XPATH, tab_path)
     print("Next_table_tab: {}".format(next_table_tab))
-    row_number,app_flag = rownum()
-    if app_flag == 1:
-        break
     if next_table_tab == 'None':
         break
     elif table_tab == 8:
+        break
+    # click on tab
+    browser.execute_script("arguments[0].click();", next_table_tab)
+    table_tab += 1
+    row_number,app_flag = rownum()
+    time.sleep(10)
+    if app_flag == 1:
         break
 
 print("application found flag {}".format(app_flag))
@@ -55,9 +61,11 @@ print("End of table search")
 app_xpath = '//*[@id="apps"]/tbody/tr[' + str(row_number - 1) + ']/td[1]/a'
 print(app_xpath)
 # perform click action over applicationID to open application history page
-click_app = browser.find_element(By.XPATH, app_xpath).click()
+click_app = browser.find_element(By.XPATH, app_xpath)
+browser.execute_script("arguments[0].click()", click_app)
 print(click_app)
 
 #//*[@id="apps_paginate"]/span/a[2]
 #//*[@id="apps_next"]
 #//*[@id="apps_paginate"]/a[2]
+#
